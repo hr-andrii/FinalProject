f96bb05b403c879d0ce9d7fb0db74cb31dad3499
hr-andrii <hr.andriygtr@gmail.com>
2019-10-21 11:38:06 +0000

    beta

--- js/function/cart.js
+++ js/function/cart.js
@@ -84,7 +84,7 @@ class Cart {
         body: JSON.stringify({
           clientName: document.querySelector('#client-name').value,
           clientEmail: document.querySelector('#client-email').value,
-          clientEmail: document.querySelector('#client-number').value,
+          clientNumber: document.querySelector('#client-number').value,
           cart: this.cart
         })
       })
